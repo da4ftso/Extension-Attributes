d62be31c599fed9fc5213cb8c0e153d90c53f0ac
da4ftso <da4@accessdenied.org>
2023-01-27 21:54:08 -0600

    Update GetChromeExtensions.sh

--- GetChromeExtensions.sh
+++ GetChromeExtensions.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 # Setting IFS Env to only use new lines as field seperator 
 IFS=$'\n'
-currentUser=`ls -l /dev/console | awk {' print $3 '}`
-lastUser=`defaults read /Library/Preferences/com.apple.loginwindow lastUserName`
+currentUser=$(/usr/bin/stat -f%Su "/dev/console")
+lastUser=$(defaults read /Library/Preferences/com.apple.loginwindow lastUserName)
 if [[ "$currentUser" = "" || "$currentUser" = "root" ]]
-    then userHome=`/usr/bin/dscl . -read /Users/$lastUser NFSHomeDirectory | awk -F ": " '{print $2}'`
-    else userHome=`/usr/bin/dscl . -read /Users/$currentUser NFSHomeDirectory | awk -F ": " '{print $2}'`
+    then userHome=$(/usr/bin/dscl . -read /Users/"$lastUser" NFSHomeDirectory | awk -F ": " '{print $2}')
+    else userHome=$(/usr/bin/dscl . -read /Users/"$currentUser" NFSHomeDirectory | awk -F ": " '{print $2}')
 fi
 createChromeExtList ()
 {
 for manifest in $(find "$userHome/Library/Application Support/Google/Chrome/Default/Extensions" -name 'manifest.json')
     do 
-        name=$(cat $manifest | grep '"name":' | awk -F "\"" '{print $4}')
-        if [[ `echo $name | grep "__MSG"` ]]
+        name=$(cat "$manifest" | grep '"name":' | awk -F "\"" '{print $4}')
+        if [[ $(echo "$name" | grep "__MSG") ]]
             then
-                msgName="\"`echo $name | awk -F '__MSG_|__' '{print $2}'`\":"
-                if [ -f $(dirname $manifest)/_locales/en/messages.json ]
-                    then reportedName=$(cat $(dirname $manifest)/_locales/en/messages.json | grep -i -A 3 "$msgName" | grep "message" | head -1 | awk -F ": " '{print $2}' | tr -d "\"")
-                elif [ -f $(dirname $manifest)/_locales/en_US/messages.json ]
-                    then reportedName=$(cat $(dirname $manifest)/_locales/en_US/messages.json | grep -i -A 3 "$msgName" | grep "message" | head -1 | awk -F ": " '{print $2}' | tr -d "\"")
+                msgName="\"$(echo "$name" | awk -F '__MSG_|__' '{print $2}')\":"
+                if [ -f $(dirname "$manifest")/_locales/en/messages.json ]
+                    then reportedName=$(cat $(dirname "$manifest")/_locales/en/messages.json | grep -i -A 3 "$msgName" | grep "message" | head -1 | awk -F ": " '{print $2}' | tr -d "\"")
+                elif [ -f $(dirname "$manifest")/_locales/en_US/messages.json ]
+                    then reportedName=$(cat $(dirname "$manifest")/_locales/en_US/messages.json | grep -i -A 3 "$msgName" | grep "message" | head -1 | awk -F ": " '{print $2}' | tr -d "\"")
                 fi
             else
-                reportedName=$(cat $manifest | grep '"name":' | awk -F "\"" '{print $4}')
+                reportedName=$(cat "$manifest" | grep '"name":' | awk -F "\"" '{print $4}')
         fi
-        version=$(cat $manifest | grep '"version":' | awk -F "\"" '{print $4}')
-        extID=$(basename $(dirname $(dirname $manifest)))
+        version=$(cat "$manifest" | grep '"version":' | awk -F "\"" '{print $4}')
+        extID=$(basename $(dirname $(dirname "$manifest")))
         
         # This is the default output style - looks nice in JSS
         # Comment out line below if you wish to use alternate output
@@ -36,7 +36,7 @@ for manifest in $(find "$userHome/Library/Application Support/Google/Chrome/Defa
  done
  }
 if [ -d "$userHome/Library/Application Support/Google/Chrome/Default/Extensions" ]
-        then result="`createChromeExtList`"
+        then result="$(createChromeExtList)"
         else result="NA"
 fi
 echo "<result>$result</result>"
