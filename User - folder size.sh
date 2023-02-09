d8909c2b45f27c4ffbf71656ea3840c4090dc7af
da4ftso <da4@accessdenied.org>
2023-02-09 16:14:11 -0600

    Update and rename User - folder size to User - folder size.sh

--- User - folder size.sh
+++ User - folder size.sh
@@ -5,11 +5,11 @@
 # github.com/acodega
 #
 
-#find logged in user
+# find logged in user
 loggedinuser=$( scutil <<< "show State:/Users/ConsoleUser" | awk -F': ' '/[[:space:]]+Name[[:space:]]:/ { if ( $2 != "loginwindow" ) { print $2 }}' )
 
-#find how large downloads is
+# find how large downloads is
 downloads=$(du -hd 1 /Users/"$loggedinuser"/Downloads | awk 'END { print $1 }')
 
-#echo it for EA
+# echo it for EA
 echo "<result>$downloads</result>"
