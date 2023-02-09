59e45b35eab25200e310d3b20ce0f30106b9cdda
da4ftso <da4@accessdenied.org>
2023-02-09 15:52:54 -0600

    Delete Display_Mobile_Accounts_EA 2.sh

--- Display_Mobile_Accounts_EA 2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-userList=$(dscl . list /Users UniqueID | awk '$2 > 1000 {print $1}')
-if [ "$userList" ]; then
-   echo "<result>$userList</result>"
-else
-   echo "<result>No mobile users</result>"
-fi
