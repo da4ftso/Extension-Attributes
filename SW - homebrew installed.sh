6b97ea48eff9032dc0bb13ea8a7c687395438868
da4ftso <da4@accessdenied.org>
2023-02-09 20:34:15 -0600

    Update SW - homebrew installed.sh
    check for brew in both arm64 and x86 locations, report version if found

--- SW - homebrew installed.sh
+++ SW - homebrew installed.sh
@@ -2,8 +2,14 @@
 
 # check for existence of homebrew
 
-if [[ -f "/usr/local/bin/brew" || -f "/opt/homebrew/bin/brew" ]]; then
-    echo "<result>Installed: $(brew config | awk '/HOMEBREW_VERSION:/ { print $NF } ')</result>"
+if [ -f "/usr/local/bin/brew" ] ; then
+
+    echo "<result>Installed: $(/usr/local/bin/brew config | awk '/HOMEBREW_VERSION:/ { print $NF } ')</result>"
+
+elif [ -f "/opt/homebrew/bin/brew" ] ; then
+
+    echo "<result>Installed: $(/opt/homebrew/bin/brew config | awk '/HOMEBREW_VERSION:/ { print $NF } ')</result>"
+    
 else
     echo "<result>Not Installed</result>"
 fi
