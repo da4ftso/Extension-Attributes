364293580b194a384538b132d7fe404602ef6bcd
da4ftso <da4@accessdenied.org>
2023-02-09 19:30:20 -0600

    Create HW - Current WiFi - Short.sh

---
+++ HW - Current WiFi - Short.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | /usr/bin/grep -w 'AirPort\|SSID' | /usr/bin/awk -F ": " '{print $2}')
+
+if [[ -z $SSID ]]; then
+	echo "<result>Not Connected</result>"
+else
+	echo "<result>${SSID}</result>"
+fi
+
+exit 0
