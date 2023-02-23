3966ff916b0e5c42ca60bfece7679c96ef6cb60a
da4ftso <da4@accessdenied.org>
2023-02-22 19:40:32 -0600

    Update EA - Version Template.sh

--- EA - Version Template.sh
+++ EA - Version Template.sh
@@ -2,6 +2,10 @@
 
 app="$4" # add the usual to deal with ".app" or not
 
+# did $4 contain .app?
+# did $4 contain a path?
+# did $4 contain a path that wasn't /Applications/ ?
+
 plist="{$app}/Contents/Info.plist"
 
 if [[ -e $plist ]]; then
