1e449adca1c444d9236e67e1e047f5c56928dc2c
da4ftso <da4@accessdenied.org>
2023-03-29 11:24:10 -0500

    Update and rename OS - ZeroTier - Verion.sh to SW - ZeroTier - Verion.sh

--- SW - ZeroTier - Verion.sh
+++ SW - ZeroTier - Verion.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 # query zerotier-cli, return version
-
 # TO-DO: check if ZeroTier is running, return that status?
-
 # check for binary, bail out
 
 if [[ -e /usr/local/bin/zerotier-cli ]]; then
 
-	version=$(/usr/local/bin/zerotier-cli info | awk ' { print $4 } ')
+	VERSION=$(/usr/local/bin/zerotier-cli info | awk ' { print $4 } ')
     
-    echo "<result>$version</result>"
+    echo "<result>$VERSION</result>"
     
 else
 
