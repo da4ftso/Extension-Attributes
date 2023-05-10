fe4b5e454971c184c2ae37a3c6491d0217940207
da4ftso <da4@accessdenied.org>
2023-05-10 12:07:47 -0500

    Update OS - Check For VPNs.sh

--- OS - Check For VPNs.sh
+++ OS - Check For VPNs.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# call scutil to look for system-level VPN connections
+# consider filtering out 'PPP' for device serial modems and other small peripherals
+# typically using this to look for things like Nord, Fortigate, Proton
+
 vpns=$(/usr/sbin/scutil --nc list | sed '1d')
 
 if [[ ${vpns} = *LPSS* ]]; then
