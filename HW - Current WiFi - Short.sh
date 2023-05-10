18fb7509e872e868fde12af41dd9e01b3636e82f
da4ftso <da4@accessdenied.org>
2023-05-10 12:08:11 -0500

    Update HW - Current WiFi - Short.sh
    xargs! XAAAARRRRRRGGGGSS

--- HW - Current WiFi - Short.sh
+++ HW - Current WiFi - Short.sh
@@ -2,7 +2,7 @@
 
 # returns "Off" if Wi-Fi is off
 
-SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | /usr/bin/awk '/SSID: / { $1 = "" ; print $0 }')
+SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | /usr/bin/awk '/SSID: / { $1 = "" ; print $0 }') | xargs
 
 if [[ -z $SSID ]]; then
 	echo "<result>Not Connected</result>"
