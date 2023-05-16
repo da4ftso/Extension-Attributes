d014ce6b522d38bd6c5c740ab732df843e9f069d
da4ftso <da4@accessdenied.org>
2023-05-16 14:45:02 -0500

    Create NW - System VPNs.sh

---
+++ NW - System VPNs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# EA - NW - Check For VPN.sh
+
+# call scutil to check for any existing system VPN configurations
+
+# typically you can ignore 'ppp' entries - more interested in Nord, L2TP, etc
+
+echo "<result>$(/usr/bin/scutil --nc list | sed '1d')</result>"
