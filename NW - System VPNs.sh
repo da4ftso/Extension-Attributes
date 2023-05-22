fb7f1825e4f2c1bbc02c91e9be1ae0e35be60d16
da4ftso <da4@accessdenied.org>
2023-05-22 13:36:27 -0500

    Update NW - System VPNs.sh

--- NW - System VPNs.sh
+++ NW - System VPNs.sh
@@ -4,6 +4,6 @@
 
 # call scutil to check for any existing system VPN configurations
 
-# typically you can ignore 'ppp' entries - more interested in Nord, L2TP, etc
+# typically you can ignore 'ppp' entries - more interested in Nord, Proton, L2TP, etc
 
 echo "<result>$(/usr/bin/scutil --nc list | sed '1d')</result>"
