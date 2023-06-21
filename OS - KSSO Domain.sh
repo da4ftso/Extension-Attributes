97834c014ea2d07d3555bb4eb40d2f1d66ec0571
da4ftso <da4@accessdenied.org>
2023-06-21 12:16:10 -0500

    Update OS - KSSO Domain.sh

--- OS - KSSO Domain.sh
+++ OS - KSSO Domain.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 # /usr/bin/app-sso is present in macOS Monterey and above (as far back as Catalina or Big Sur?)
+# but on Monterey, output seems to be different -
+# try parsing the .plist instead:
+# defaults read //Users/$loggedInUser/Library/Group\ Containers/group.com.apple.KerberosExtension/Library/Preferences/group.com.apple.KerberosExtension.plist DOMAIN.TLD:userName
 
 # reports status from devices logged in to AD via Kerberos SSO extension
 
