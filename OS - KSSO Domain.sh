88c36bd7e1b64d1d11576fe27bcef718d9896bd7
da4ftso <da4@accessdenied.org>
2023-08-26 09:00:26 -0500

    Update OS - KSSO Domain.sh
    rolled tr into awk gsub

--- OS - KSSO Domain.sh
+++ OS - KSSO Domain.sh
@@ -3,12 +3,12 @@
 # /usr/bin/app-sso is present in macOS Monterey and above (as far back as Catalina or Big Sur?)
 # but on Monterey, output seems to be different -
 # try parsing the .plist instead:
-# defaults read //Users/$loggedInUser/Library/Group\ Containers/group.com.apple.KerberosExtension/Library/Preferences/group.com.apple.KerberosExtension.plist DOMAIN.TLD:userName
+# defaults read /Users/$loggedInUser/Library/Group\ Containers/group.com.apple.KerberosExtension/Library/Preferences/group.com.apple.KerberosExtension.plist DOMAIN.TLD:userName
 
 # reports status from devices logged in to AD via Kerberos SSO extension
 
-# domain
+# domain=$(/usr/bin/app-sso -l -j | awk '/"/ { print $NF } ' | tr -d '",')
 
-domain=$(/usr/bin/app-sso -l -j | awk '/"/ { print $NF } ' | tr -d '",')
+domain=$(/usr/bin/app-sso -l -j | awk '/"/ { print substr($1, 2, length($1) - 2) }')
 
 echo "<result>${domain}</result>"
