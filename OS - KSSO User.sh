ef2e14d4fa8ffec49f267cfe36113df0ec8c8ff0
da4ftso <da4@accessdenied.org>
2023-08-26 09:08:25 -0500

    Update OS - KSSO User.sh
    rolled tr into awk substr

--- OS - KSSO User.sh
+++ OS - KSSO User.sh
@@ -4,15 +4,14 @@
 # but on Monterey, output seems to be different - try parsing the .plist instead:
 # defaults read //Users/$loggedInUser/Library/Group\ Containers/group.com.apple.KerberosExtension/Library/Preferences/group.com.apple.KerberosExtension.plist DOMAIN.TLD:userName
 
-
 # reports status from devices logged in to AD via Kerberos SSO extension
 
 # domain
 
-domain=$(/usr/bin/app-sso -l -j | awk '/"/ { print $NF } ' | tr -d '",')
+domain=$(/usr/bin/app-sso -l -j | awk '/"/ { print substr($1, 2, length($1) - 2) }')
 
 # user_name
 
-user=$(/usr/bin/app-sso -i $domain -j | awk '/user_name/ { print $NF } ' | tr -d '",')
+user=$(/usr/bin/app-sso -i $domain -j | awk '/user_name/ { print substr($NF, 2, length($NF) - 3) }')
 
 echo "<result>${user}</result>"
