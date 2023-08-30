25ecab2a0a51024de431984e1cfd3870d15a9cc3
da4ftso <da4@accessdenied.org>
2023-08-30 11:15:10 -0500

    Update NW - Current WiFi - Short.sh
    better newline handling

--- NW - Current WiFi - Short.sh
+++ NW - Current WiFi - Short.sh
@@ -2,7 +2,7 @@
 
 # returns "Off" if Wi-Fi is off
 
-SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | /usr/bin/awk '/SSID: / { $1 = "" ; print $0 }') | xargs
+SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | /usr/bin/awk '/SSID: / { $1 = "" ; print $0 }') | tr -d '/n' | xargs
 
 if [[ -z $SSID ]]; then
 	echo "<result>Not Connected</result>"
