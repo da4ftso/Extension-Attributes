8358db16ddd57e002a0a42ca0d9fc26bb7ab4cdd
da4ftso <da4@accessdenied.org>
2023-09-02 07:53:22 -0500

    Update NW - Current WiFi - Short.sh

--- NW - Current WiFi - Short.sh
+++ NW - Current WiFi - Short.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# 1.1 230830
 # returns "Off" if Wi-Fi is off
 
 SSID=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | /usr/bin/awk '/SSID: / { $1 = "" ; print $0 }') | tr -d '/n' | xargs
