cfeda63399b960320edb296a218f9e7af0815ca5
da4 <da4@accessdenied.org>
2023-10-26 13:21:26 -0500

    Create OS - Bluetooth Sharing Status.sh

---
+++ OS - Bluetooth Sharing Status.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# return current status of Bluetooth Sharing
+
+currentUser=$(stat -f%Su /dev/console)
+
+state=$(sudo -u "$currentUser" defaults -currentHost read com.apple.Bluetooth PrefKeyServicesEnabled)
+
+if [ $state = "1" ]; then
+	status="Enabled"
+
+elif [ $state = "0" ]; then
+	status="Disabled"
+
+fi    
+
+echo "<result>$status</result>"
+
+exit 0
