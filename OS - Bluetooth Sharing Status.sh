e527a0929316abd906920a216d2429dc7ee655c9
da4 <da4@accessdenied.org>
2023-10-26 17:20:02 -0500

    Update OS - Bluetooth Sharing Status.sh
    credits to billabOng for helping verify behavior

--- OS - Bluetooth Sharing Status.sh
+++ OS - Bluetooth Sharing Status.sh
@@ -3,6 +3,10 @@
 # return current status of Bluetooth Sharing
 
 # Bluetooth Sharing must have been previously enabled for this to return any value regardless of current status
+# otherwise, a non-zero value will be returned: 
+#
+#   The domain/default pair of (com.apple.Bluetooth, PrefKeyServicesEnabled) does not exist
+
 
 currentUser=$(stat -f%Su /dev/console)
 
