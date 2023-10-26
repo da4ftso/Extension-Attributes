5f313fb773fdfb29becfa24a1ec6715f17c8a3ed
da4 <da4@accessdenied.org>
2023-10-26 16:59:49 -0500

    Update OS - Bluetooth Sharing Status.sh

--- OS - Bluetooth Sharing Status.sh
+++ OS - Bluetooth Sharing Status.sh
@@ -2,6 +2,8 @@
 
 # return current status of Bluetooth Sharing
 
+# Bluetooth Sharing must have been previously enabled for this to return any value regardless of current status
+
 currentUser=$(stat -f%Su /dev/console)
 
 state=$(sudo -u "$currentUser" defaults -currentHost read com.apple.Bluetooth PrefKeyServicesEnabled)
