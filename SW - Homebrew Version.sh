6335aaa10dbe64d9744136778911b0fa3d6965b9
da4 <da4@accessdenied.org>
2023-11-27 15:31:57 -0600

    Update SW - Homebrew Version.sh
    changed from $NF to $2

--- SW - Homebrew Version.sh
+++ SW - Homebrew Version.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
 
+# with no awk, we'd get some crud:
+#  Homebrew 4.1.15 Homebrew/homebrew-core (no Git repository) Homebrew/homebrew-cask (git revision 9d67b1d07b; last commit 2023-10-10)
+#
+# with awk $NF we'd get a partial:
+#  repository)
+#
+# with awk $2 we (typically) get a usable value:
+#   >=2.5.0 4.1.15 4.1.21
+
+
 loggedInUser=$(/usr/bin/stat -f%Su "/dev/console")
 
 architectureCheck=$(/usr/bin/uname -m)
@@ -17,10 +27,10 @@ brewPath="$brewPrefix/brew"
 # Check for presence of target binary and get version.
 
 if [ -e "$brewPath" ]; then
-  result=$(sudo -u "$loggedInUser" "$brewPath" -v | awk '{ print $NF } ')
+  result=$(sudo -u "$loggedInUser" "$brewPath" --version | awk ' { print $2 }' )
 
 else
-  result=""
+  result="" # change here if you'd prefer a label ie 'Not Installed'
 
 fi
 
