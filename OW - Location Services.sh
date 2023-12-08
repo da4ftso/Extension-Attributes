039263b45fee7345e0b8cd0c40abd0c32229effa
da4 <da4@accessdenied.org>
2023-12-08 11:40:01 -0600

    Create OW - Location Services.sh
    1.0 if no key in the plist, will report Disabled

---
+++ OW - Location Services.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+uuid=$(system_profiler SPHardwareDataType | grep "Hardware UUID" | awk '{print $3}')
+domain="/var/db/locationd/Library/Preferences/ByHost/com.apple.locationd.${uuid}"
+plist="${domain}.plist"
+
+sw_vers=$(sw_vers -productVersion | awk -F. '{print $1}') # 14 for Sonoma, eg
+
+if [[ -f "${plist}" ]]
+then
+	if [ "$sw_vers" -gt "11" ]
+	then
+	        status=$(plutil -p "${domain}" | awk '/LocationServicesEnabled/ {print $NF}')
+	else
+                status=$(defaults read "${domain}" LocationServicesEnabled)
+        fi
+    if [[ "${status}" == "1" ]]
+    then
+        result="Enabled"
+    else
+        result="Disabled"
+    fi
+else
+    result="Unavailable"
+fi
+
+echo "<result>${result}</result>"
