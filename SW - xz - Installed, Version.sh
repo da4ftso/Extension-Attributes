d75bc3eda7c0df8fe11f591e0d4d74b10501cd5c
da4 <da4@accessdenied.org>
2024-04-01 19:02:21 -0500

    Create SW - xz - Installed, Version.sh
    check for xz, return the currently installed version if present

---
+++ SW - xz - Installed, Version.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+architectureCheck=$(/usr/bin/uname -m)
+
+if [ "$architectureCheck" = "arm64" ]; then
+  brewPrefix="/opt/homebrew/bin"
+
+else
+  brewPrefix="/usr/local/bin"
+
+fi
+
+brewPath="$brewPrefix/brew"
+
+"$brewPath" list | /usr/bin/grep xz
+
+if [ $? -eq 0 ]; then
+  version=$(brew info xz | awk 'NR==1 {print $4}')
+  echo "<result>Installed: $version</result>"
+else
+  echo "<result>Not Installed</result>"
+fi
