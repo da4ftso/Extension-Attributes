cf9e61dd1923ffc11e9c710e35b56fba02fb4c64
da4 <da4@accessdenied.org>
2024-04-02 12:44:29 -0500

    Update SW - xz - Installed, Version.sh
    runAsUser and 2 if loops

--- SW - xz - Installed, Version.sh
+++ SW - xz - Installed, Version.sh
@@ -1,24 +1,30 @@
-#/bin/bash
+#!/bin/bash
 
+# checks for brew; if present, checks for xz; if present, returns the version
+# otherwise returns "Not Installed"
+#
 # https://access.redhat.com/security/cve/CVE-2024-3094
+
+# runAsUser since otherwise Jamf will report this via the root user and give an inaccurate result
+loggedInUser=$(/usr/bin/stat -f%Su "/dev/console")
 
 architectureCheck=$(/usr/bin/uname -m)
 
-if [ "$architectureCheck" = "arm64" ]; then
+if [[ "$architectureCheck" = "arm64" ]]; then
   brewPrefix="/opt/homebrew/bin"
-
 else
   brewPrefix="/usr/local/bin"
-
 fi
 
 brewPath="$brewPrefix/brew"
 
-"$brewPath" list | /usr/bin/grep xz
+if [[ -e "$brewPath" ]]; then
+  result=$(sudo -u "$loggedInUser" "$brewPath" list | grep xz)
 
-if [ $? -eq 0 ]; then
-  version=$(brew info xz | awk 'NR==1 {print $4}')
-  echo "<result>Installed: $version</result>"
-else
-  echo "<result>Not Installed</result>"
+  if [[ "$result" != "" ]]; then 
+		version=$(sudo -u "$loggedInUser" "$brewPath" info xz | awk 'NR==1 { print $4 }')
+  		echo "<result>Installed: $version</result>"
+        else
+                echo "<result>Not Installed</result>"
+  fi
 fi
