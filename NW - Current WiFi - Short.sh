5843e5095c353a9736e821d202b84cd5aa7e5073
da4 <da4@accessdenied.org>
2024-09-05 10:37:41 -0500

    Update NW - Current WiFi - Short.sh
    if you're looking for a script snippet, wdutil requires sudo

--- NW - Current WiFi - Short.sh
+++ NW - Current WiFi - Short.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# if you're looking for a script snippet, remember that wdutil requires sudo
+
 # 1.3 240808 new version that supports 14+
 # 1.2 230924 changed to NF and rm'd xargs
 # 1.1 230830
