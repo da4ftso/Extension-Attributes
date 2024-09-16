9ff9a1822f060b3f7cf675c9cd1e195e65b7d346
da4 <da4@accessdenied.org>
2024-09-16 13:19:19 -0500

    Update NW - Current WiFi.sh
    since Sequoia RC, wdutil returns 'redacted' so parsing the array instead

--- NW - Current WiFi.sh
+++ NW - Current WiFi.sh
@@ -8,16 +8,16 @@ osProductVersion=$( /usr/bin/sw_vers -productVersion )
 case "${osProductVersion}" in
 
     12* | 13* | 14* )
-	wifi=$(networksetup -listallhardwareports | awk '/Hardware Port: Wi-Fi/,/Ethernet/' | awk 'NR==2' | cut -d " " -f 2)
-	ssid=$(networksetup -getairportnetwork "$wifi" | awk -F': ' '{ print $NF;exit }')
-    		if [[ $ssid == *"not associated"* ]]; then
-		ssid="None"
-		fi
-	echo "<result>${ssid}</result>"
+		wifi=$(networksetup -listallhardwareports | awk '/Hardware Port: Wi-Fi/,/Ethernet/' | awk 'NR==2' | cut -d " " -f 2)
+		ssid=$(networksetup -getairportnetwork "$wifi" | awk -F': ' '{ print $NF;exit }')
+    if [[ $ssid == *"not associated"* ]]; then
+      ssid="None"
+      fi
+		echo "<result>${ssid}</result>"
         ;;
 
     15* )
-    	ssid=$(wdutil info | awk -F: '/SSID/ { print $NF;exit } ') ; ssid=${ssid:1}
+    	ssid=$(/usr/libexec/PlistBuddy -c "Print :0:_items:spairport_airport_interfaces:spairport_airport_interfaces:spairport_current_network_information:spairport_current_network_information:_name" /dev/stdin <<< $(system_profiler SPAirPortDataType -xml) 2> /dev/null )
         echo "<result>${ssid}</result>"
         ;;
 
