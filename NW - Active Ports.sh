b66db1d2d6c7ed0ef4aa9e5aa0ea3aace921e656
da4 <da4@accessdenied.org>
2025-01-02 11:32:40 -0600

    Update NW - Active Ports.sh

--- NW - Active Ports.sh
+++ NW - Active Ports.sh
@@ -2,6 +2,12 @@
 
 # Get all hardware ports and their interfaces
 
+# sample output:
+# <result>Belkin USB-C LAN (en8)
+# Wi-Fi (en0)</result>
+#
+# modern JSS will print them on a single line, nothing to worry about
+
 interfaces=$(networksetup -listallhardwareports | awk '
 /Hardware Port/ { port=substr($0, index($0, $3)) }
 /Device/ { device=$2 }
