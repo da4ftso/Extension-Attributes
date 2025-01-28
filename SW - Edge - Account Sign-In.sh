cbc17841d01eded64c5875271127ca74f5e31328
da4 <da4@accessdenied.org>
2025-01-28 13:09:53 -0600

    Update SW - Edge - Account Sign-In.sh
    1.1 loop through all profiles, report email signed in

--- SW - Edge - Account Sign-In.sh
+++ SW - Edge - Account Sign-In.sh
@@ -1,20 +1,41 @@
 #!/bin/bash
 
-# TO-DO: loop through multiple profiles
+# 1.1 250128 PWC Happy Five O'Clock Day, 312!
+
+# This version now iterates through all Edge profiles and reports the profile and
+# the email address signed in.
+
+# Since jq is now included by default on macOS we can use that more effectively than
+# trying to parse the JSON with awk.
 
 # Setting IFS Env to only use new lines as field seperator 
 IFS=$'\n'
 
-currentUser=$(/usr/bin/stat -f%Su "/dev/console") # pete
+# should probably bail out if jq not found
+
+currentUser=$(/usr/bin/stat -f%Su "/dev/console") # u446852
 lastUser=$(defaults read /Library/Preferences/com.apple.loginwindow lastUserName)
 
 if [[ "$currentUser" = "" || "$currentUser" = "root" ]]
     then userHome=$(/usr/bin/dscl . -read /Users/"$lastUser" NFSHomeDirectory | awk -F ": " '{print $2}')
-    else userHome=$(/usr/bin/dscl . -read /Users/"$currentUser" NFSHomeDirectory | awk -F ": " '{print $2}') # /Users/pete
+    else userHome=$(/usr/bin/dscl . -read /Users/"$currentUser" NFSHomeDirectory | awk -F ": " '{print $2}') # /Users/u446852
 fi
 
-if grep -q gaia "${userHome}"/Library/Application\ Support/Microsoft\ Edge/Default/Preferences ; then
-	echo "An account is signed in to Edge..."
+getEmail() {
+for Preferences in $(find "${userHome}"/Library/Application\ Support/Microsoft\ Edge/*/Preferences )
+	do
+		email=$(jq -r '.account_info[0].email' $Preferences )
+			if [ $email != "null" ]; then
+				profileName=$(echo $Preferences | awk -F'/' '/Edge/ { print $7 }' )
+				echo $profileName": "$email
+			fi	
+	done
+}
+
+if [ -d "${userHome}"/Library/Application\ Support/Microsoft\ Edge/ ]; then
+	result="$(getEmail)"
 else
-	echo "No sign-in detected..."
-fi	
+	result=""
+fi		
+
+echo "<result>"$result"</result>"
