70583b30ac89dde64e0034cbad5c2be799b90de5
da4 <da4@accessdenied.org>
2025-02-04 08:51:32 -0600

    Update HW - Bluetooth Devices.sh
    both connected and all

--- HW - Bluetooth Devices.sh
+++ HW - Bluetooth Devices.sh
@@ -1,14 +1,25 @@
 #!/bin/zsh
 
-# this version returns both connected and disconnected devices.
+# both versions here
 
 # I am so sorry for this. please don't hurt me.
-
 # old method, slow, heinous
 # btdevices=$(system_profiler SPBluetoothDataType | grep -E 'Type:.*Headphones|Keyboard|Mouse' -C 4 | sed -e 's/0x004C/Apple/' -e 's/0x009E/Bose/' -e 's/0x04E8/Samsung/' -e 's/0x000A/JBL/' -e 's/0x054C/Sony/' -e 's/0x046D/Logi/' -e 's/0x0055/Platronics/' -e 's/0x0094/JLab/' | awk '!/Address|Product|Case|Firmware|Serial/' | tr -s ' ' | sed -E 's/Vendor ID:|Minor Type:|- Find My:| Battery Level|Transport: |Mouse Services:|Keyboard Services:|UART|HID|ACL|ANC|BLE|0x800020|0x1800019|0x800019|0x1980019|0x400000|GATT|HFP|AVRCP|A2DP|SCO|PCIe//g' | tr -d '\n')
 
-# new hotness
+# all devices
 
-btdevices=$(/usr/bin/wdutil info | awk '/Address/ { print prev } { prev = $0 }' | tail -n +6 | tr -s ' ' | sed '$d' | sed 's/‚Äô/'\''/g')
+btdevices=$(/usr/bin/wdutil info | \
+  awk '/Address/ { print prev } { prev = $0 }' | \
+  tail -n +6 | tr -s ' ' | sed '$d' | sed 's/‚Äô/'\''/g')
+
+# connected devices only
+
+btdevices=$(/usr/bin/wdutil info | \
+  awk '$0 ~ /Connected[[:space:]]*:[[:space:]]*Yes/ { print dev }
+       $0 ~ /Address/ { dev = prev }
+       { prev = $0 }' | \
+  sed 's/‚Äô/'\''/g')
+
+# results
 
 echo "<result>${btdevices}</result>"
