66e271276770755d163f396ec4ffa0df402f182c
da4 <da4@accessdenied.org>
2025-02-04 08:29:57 -0600

    Update SW - Edge - Account Sign-In.sh

--- SW - Edge - Account Sign-In.sh
+++ SW - Edge - Account Sign-In.sh
@@ -3,22 +3,22 @@
 # 1.1 250128 PWC Happy Five O'Clock Day, 312!
 
 # This version now iterates through all Edge profiles and reports the profile and
-# the email address signed in.
+# the email address signed in. If Edge has never been used, this will return nothing.
 
-# Since jq is now included by default on macOS we can use that more effectively than
-# trying to parse the JSON with awk.
+# Since jq is now included by default on macOS we can use that more effectively
+# then trying to parse the JSON with awk.
 
 # Setting IFS Env to only use new lines as field seperator 
 IFS=$'\n'
 
 # should probably bail out if jq not found
 
-currentUser=$(/usr/bin/stat -f%Su "/dev/console") # u446852
+currentUser=$(/usr/bin/stat -f%Su "/dev/console")
 lastUser=$(defaults read /Library/Preferences/com.apple.loginwindow lastUserName)
 
 if [[ "$currentUser" = "" || "$currentUser" = "root" ]]
     then userHome=$(/usr/bin/dscl . -read /Users/"$lastUser" NFSHomeDirectory | awk -F ": " '{print $2}')
-    else userHome=$(/usr/bin/dscl . -read /Users/"$currentUser" NFSHomeDirectory | awk -F ": " '{print $2}') # /Users/u446852
+    else userHome=$(/usr/bin/dscl . -read /Users/"$currentUser" NFSHomeDirectory | awk -F ": " '{print $2}')
 fi
 
 getEmail() {
