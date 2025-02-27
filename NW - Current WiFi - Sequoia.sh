1846ea41b8975a4563c583d21ddf64994e572653
da4 <da4@accessdenied.org>
2025-02-27 10:35:58 -0600

    Update NW - Current WiFi - Sequoia.sh

--- NW - Current WiFi - Sequoia.sh
+++ NW - Current WiFi - Sequoia.sh
@@ -2,14 +2,17 @@
 
 # if you're looking for a script snippet, remember that wdutil requires sudo
 
+# 1.5 250227 snelson was right, this is better
 # 1.4 240916 since RC, wdutil now returns 'redacted' so we have to parse the array regardless
 # 1.3 240808 new version that supports 14+
 # 1.2 230924 changed to NF and rm'd xargs
 # 1.1 230830
 
+# SSID=$(/usr/libexec/PlistBuddy -c "Print :0:_items:spairport_airport_interfaces:spairport_airport_interfaces:spairport_current_network_information:spairport_current_network_information:_name" /dev/stdin <<< $(system_profiler SPAirPortDataType -xml) 2> /dev/null )
+# SSID=${SSID:1}
 
-SSID=$(/usr/libexec/PlistBuddy -c "Print :0:_items:spairport_airport_interfaces:spairport_airport_interfaces:spairport_current_network_information:spairport_current_network_information:_name" /dev/stdin <<< $(system_profiler SPAirPortDataType -xml) 2> /dev/null )
-SSID=${SSID:1}
+SSID=$(ipconfig getsummary $(networksetup -listallhardwareports | awk '/Hardware Port: Wi-Fi/{getline; print $2}') | awk -F ' SSID : ' '/ SSID : / {print $2}')
+
 
 if [[ -z $SSID ]]; then
         echo "<result>Not Connected</result>"
