7ef780280949c855604fd7b33ea92b0f3bed985d
da4 <da4@accessdenied.org>
2025-04-24 11:30:47 -0500

    Update EA - node version.sh

--- EA - node version.sh
+++ EA - node version.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# 1.0.1 250424 PWC node versions
-#  ditch the else under brew node check?
+# 1.0.2 250424 node versions
 
 loggedInUser=$(/usr/bin/stat -f%Su "/dev/console")
 architectureCheck=$(/usr/bin/uname -m)
+currentUserHome=$(dscl . read "/Users/loggedInUser" NFSHomeDirectory | cut -d: -f 2 | sed 's/^ *//'| tr -d '\n')
 
 # which node?
 #  brew:     /opt/homebrew/bin/node
 #  nvm/fnm:  /Users/userhome/.nvm/versions/node/v22.15.0/bin/node
 
-installedNode=$(which node)
+# installedNode=$(which node)
 
 # brew node
-# brewNodex() {
+
 if [ "$architectureCheck" = "arm64" ]; then
   brewPrefix="/opt/homebrew/bin"
 
@@ -27,28 +27,23 @@ brewPath="$brewPrefix/brew"
 # Check for presence of target binary and get version.
 
 if [ -e "$brewPath" ]; then
-
-	info=$(sudo -u "$loggedInUser" "$brewPath" info jenkins)
-
-		if [[ $info == *"Installed"* ]]; then
+	info=$(sudo -u "$loggedInUser" "$brewPath" info node)
+		if [[ $info == "installed" ]]; then
 		# sudo -u "$loggedInUser" "$brewPath" upgrade -f node
         info=$(sudo -u "$loggedInUser" "$brewPath" info node)
-    	result="$( echo "$info" | awk '/node:/ { print $2 " " $3 " " $4;exit }')"
-    	
+    	result="$( echo "$info" | awk '/node:/ { print $2 " " $3 " " $4;exit }')"	
     	fi
-
 else
   result="" # change here if you'd prefer a label ie 'Not Installed'
 
 fi
-# }
 
 # npm node
-# npmNode{} (
+
 if [ -e "${loggedInUser}"/.nvm/ ]; then
-   result=$(sudo -u "$loggedInUser" node -v)
-fi   
-# )
+	latest=$(ls -r .nvm/versions/node/ | awk ' { print $1; exit } ' )
+   result=$(sudo -u "$loggedInUser" "$currentUserHome"/.nvm/versions/node/"$latest"/bin/node -v)
+fi
 
 # if brew node vs npm node, get $result
 
