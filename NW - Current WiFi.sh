b85a101bdcca6dc36ca454da0722a7e0012f1a19
da4 <da4@accessdenied.org>
2025-08-21 11:01:11 -0500

    Update NW - Current WiFi.sh
    added Tahoe support

--- NW - Current WiFi.sh
+++ NW - Current WiFi.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# Report current SSID, or "None"
+# 15.7 apparently still returns <redacted>
+# 26.0 currently returns the SSID name, something to watch for RC & beyond
 
 osProductVersion=$( /usr/bin/sw_vers -productVersion )
+
 
 case "${osProductVersion}" in
 
     12* | 13* | 14* )
-		wifi=$(wifi=$(networksetup -listallhardwareports | awk '/Hardware Port: Wi-Fi/{getline; print $NF}')
+		wifi=$(networksetup -listallhardwareports | awk '/Hardware Port: Wi-Fi/,/Ethernet/' | awk 'NR==2' | cut -d " " -f 2)
 		ssid=$(networksetup -getairportnetwork "$wifi" | awk -F': ' '{ print $NF;exit }')
-    if [[ $ssid == *"not associated"* ]]; then
-      ssid="None"
-      fi
 		echo "<result>${ssid}</result>"
         ;;
 
@@ -19,7 +18,10 @@ case "${osProductVersion}" in
     	ssid=$(/usr/libexec/PlistBuddy -c "Print :0:_items:spairport_airport_interfaces:spairport_airport_interfaces:spairport_current_network_information:spairport_current_network_information:_name" /dev/stdin <<< $(system_profiler SPAirPortDataType -xml) 2> /dev/null )
         echo "<result>${ssid}</result>"
         ;;
-
+	26* )
+		ssid=$(/usr/bin/wdutil info | grep -w SSID | sed 's/.*\: //')
+		echo "<result>${ssid}</result>"
+		;;        
 esac
 
 exit 0
