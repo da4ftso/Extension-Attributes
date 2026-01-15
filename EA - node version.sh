ceb23befa5c5cd8d09fbf3cd51255789814396ba
da4 <da4@accessdenied.org>
2026-01-15 12:34:13 -0600

    Update EA - node version.sh
    applying standards, ie loggedInUser -> currentUser and new-style currentUserHome

--- EA - node version.sh
+++ EA - node version.sh
@@ -5,9 +5,10 @@
 # running an EA from Jamf Pro runs as root, so we have to call brew based on the logged in account
 
 result=""
-loggedInUser=$(/usr/bin/stat -f%Su "/dev/console")
+currentUser=$(/usr/bin/stat -f%Su "/dev/console")
+currentUserHome=$(eval echo "~$currentUser")
+# currentUserHome=$(dscl . read /Users/"${currentUser}" NFSHomeDirectory | cut -d: -f 2 | sed 's/^ *//'| tr -d '\n')
 architectureCheck=$(/usr/bin/uname -m)
-currentUserHome=$(dscl . read /Users/"${loggedInUser}" NFSHomeDirectory | cut -d: -f 2 | sed 's/^ *//'| tr -d '\n')
 
 # which node?
 #  brew:     /opt/homebrew/bin/node
@@ -27,13 +28,13 @@ brewPath="$brewPrefix/brew"
 
 # Check for presence of target binary and get version.
 if [ -e "$brewPath" ]; then
-	info=$(sudo -u "$loggedInUser" "$brewPath" info node)
+	info=$(sudo -u "$currentUser" "$brewPath" info node)
 	if [[ $info == *"Not installed"* ]]; then
 		result=""
 	else	
-		# sudo -u "$loggedInUser" "$brewPath" upgrade -f node
-        # info=$(sudo -u "$loggedInUser" "$brewPath" info node)
-		info=$(sudo -u "$loggedInUser" "$brewPath" info node)
+		# sudo -u "$currentUser" "$brewPath" upgrade -f node
+        # info=$(sudo -u "$currentUser" "$brewPath" info node)
+		info=$(sudo -u "$currentUser" "$brewPath" info node)
     	result="$( echo "$info" | awk '/node:/ { print $2 " " $3 " " $4;exit }' | tr -d '\r')"
     	result="${result//[!0-9.]/}" # bash only, if using zsh change this to sed
 		echo "<result>brew: $result</result>"
