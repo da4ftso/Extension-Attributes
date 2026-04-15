3e531280ab23f1c996fb2a7ff6599f344df3509c
da4 <da4@accessdenied.org>
2026-04-15 09:16:13 -0500

    Update OS - TouchID.sh

--- OS - TouchID.sh
+++ OS - TouchID.sh
@@ -4,7 +4,7 @@
 
 ### Checks to see if TouchID is enabled and returns the number of enrolled fingerprints per user if it is
 
-touchidstatus=`sudo bioutil -s -c | sed 's/Operation performed successfully.//g'`
+touchidstatus=$(sudo bioutil -s -c | sed 's/Operation performed successfully.//g')
 
 if [ "$touchidstatus" != "There are no fingerprints in the system." ]; then
 	echo "<result>$touchidstatus</result>"
