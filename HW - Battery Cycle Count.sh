8587bca264d2d5c5da1032aba34a3380290e37a2
da4 <da4@accessdenied.org>
2026-04-22 11:48:31 -0500

    Update HW - Battery Cycle Count.sh

--- HW - Battery Cycle Count.sh
+++ HW - Battery Cycle Count.sh
@@ -3,7 +3,9 @@
 # return battery cycle count, or blank if not a portable
 # use Integer as data type for better smart groups
 
-hw=$(system_profiler SPHardwareDataType | awk '/Model Name/ {print $3, $4}') # MacBook Pro
+# see Jamf repo for a one-off script that returns cycle & status
+
+hw=$(system_profiler SPHardwareDataType 2&>/dev/null | awk '/Model Name/ {print $3, $4}') # MacBook Pro
 
 if [[ $hw == MacBook* ]]; then
 	count=$(system_profiler SPPowerDataType | awk '/Cycle Count:/ { print $NF }')
