04e410b2236c56870f4f7457d93412cfbaf03954
da4 <da4@accessdenied.org>
2026-04-24 08:53:42 -0500

    Create SW - Node Versions.sh

---
+++ SW - Node Versions.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# 1.0.5 250424 node versions
+
+# running an EA from Jamf Pro runs as root, so we have to call brew based on the logged in account
+
+result=""
+loggedInUser=$(/usr/bin/stat -f%Su "/dev/console")
+architectureCheck=$(/usr/bin/uname -m)
+currentUserHome=$(dscl . read /Users/"${loggedInUser}" NFSHomeDirectory | cut -d: -f 2 | sed 's/^ *//'| tr -d '\n')
+
+# which node?
+#  brew:     /opt/homebrew/bin/node
+#  nvm/fnm:  /Users/userhome/.nvm/versions/node/v22.15.0/bin/node
+#  pkg:      /usr/local/bin/node (arm64 included)
+
+# installedNode=$(which node)
+
+# brew node
+if [ "$architectureCheck" = "arm64" ]; then
+  brewPrefix="/opt/homebrew/bin"
+else
+  brewPrefix="/usr/local/bin"
+fi
+
+brewPath="$brewPrefix/brew"
+
+# Check for presence of target binary and get version.
+if [ -e "$brewPath" ]; then
+	info=$(sudo -u "$loggedInUser" "$brewPath" info node)
+	if [[ $info == *"Not installed"* ]]; then
+		result=""
+	else	
+		# sudo -u "$loggedInUser" "$brewPath" upgrade -f node
+		info=$(sudo -u "$loggedInUser" "$brewPath" info node)
+    	result="$( echo "$info" | awk '/node:/ { print $2 " " $3 " " $4;exit }' | tr -d '\r')"
+    	result="${result//[!0-9.]/}" # bash only, if using zsh change this to sed
+		echo "<result>brew: $result</result>"
+		exit 0
+	fi
+fi
+
+# npm node
+if [ -e "${currentUserHome}"/.nvm/ ]; then
+	latest=$(ls -r "${currentUserHome}"/.nvm/versions/node/ | awk ' { print $1; exit } ' )
+	result=$("$currentUserHome"/.nvm/versions/node/"$latest"/bin/node -v | tr -d '\r')
+	result="${result//[!0-9.]/}" # bash only, if using zsh change this to sed
+	echo "<result>npm: $result</result>"
+	exit 0
+fi
+
+# pkg node
+if [ -e /usr/local/bin/node ]; then 
+	result=$(/usr/local/bin/node -v | tr -d '\r')
+ 	result="${result//[!0-9.]/}"
+  	echo "<result>pkg: $result</result>"
+fi   
