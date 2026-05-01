70a92aa3ef6b353a454da1b217025da686b4d6f6
da4 <da4@accessdenied.org>
2026-05-01 09:11:06 -0500

    Update SW - Java JDKs.sh
    consolidated to single awk

--- SW - Java JDKs.sh
+++ SW - Java JDKs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# Prettyprint all Java JDKs (VMs)
+# 1.0.3 260501
+#
+# Prettyprint all Java VMs
 #
 # /usr/libexec/java_home --verbose will return something like:
 #
@@ -9,13 +11,13 @@
 #    11.0.19 (arm64) "Amazon.com Inc." - "Amazon Corretto 11" /Library/Java/JavaVirtualMachines/amazon-corretto-11.jdk/Contents/Home
 #    11.0.15 (x86_64) "Oracle Corporation" - "Java SE 11.0.15" /Library/Java/JavaVirtualMachines/jdk-11.0.15.jdk/Contents/Home
 #    1.8.371.11 (x86_64) "Oracle Corporation" - "Java" /Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home
-#    1.8.0_341 (x86_64) "Oracle Corporation" - "Java SE 8" /Library/Java/JavaVirtualMachines/jdk1.8.0_341.jdk/Contents/Home
 # /Library/Java/JavaVirtualMachines/amazon-corretto-11.jdk/Contents/Home
-#
-# awk/sed/tr etc all the things for results like so:
-#    Azul 17.0.7 x86_64
-#    Oracle 1.8.391.13 arm64
 
-result=$(/usr/libexec/java_home --verbose 2>&1 | awk ' { print $3 " " $1 " " $2  } ' | sed '1d;$d' | tr -d '()"')
+result=$(/usr/libexec/java_home --verbose 2>&1 |
+  awk 'NR>1 { gsub(/[()"]/, ""); print $3, $1, $2 }')
+
+if [[ "$result" =~ Please ]] ; then
+	result="Not Found"
+fi
 
 printf "<result>$result</result>"
