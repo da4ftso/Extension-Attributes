fc666bffcc5be52e5f5c9da567dfc152926135a2
da4 <da4@accessdenied.org>
2026-05-06 11:48:01 -0500

    Update EA - If Template (basic).sh

--- EA - If Template (basic).sh
+++ EA - If Template (basic).sh
@@ -2,7 +2,7 @@
 
 # https://www.gnu.org/software/bash/manual/html_node/Bash-Conditional-Expressions.html
 
-if [ -f "/path/to/something" ] ; then
+if [ -f "/path/to/something" ] ; then # use -d for a directory
     echo "<result>Installed</result>"
 else
     echo "<result>Not Installed</result>"
