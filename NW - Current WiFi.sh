c2acfa906d6295069388ec92a355930e2731974f
da4 <da4@accessdenied.org>
2026-09-18 14:19:51 -0500

    Update NW - Current WiFi.sh

--- NW - Current WiFi.sh
+++ NW - Current WiFi.sh
@@ -3,14 +3,12 @@
 # 15.7 apparently still returns <redacted>
 # 26.0 currently returns the SSID name, something to watch for RC & beyond
 # 26.0.1 and later started returning <redacted>
-# this version correctly returns on 26.2 and 26.3b1
+# this version correctly returns on 26.2+ incl 27.0
 # credit to ZP and Dan K Snelson for the ipconfig setverbose
-
-# file Feedback with Apple as to why this needs to be set via MDM!
 
 iface=$(scutil --nwi | grep -E -om1 'en\d') # egrep is deprecated
 
-/usr/sbin/ipconfig setverbose 1
+# /usr/sbin/ipconfig setverbose 1 - doesn't work in GG
 
 ssid=$(ipconfig getsummary $iface | awk '/ SSID : / { print $NF } ')
 
