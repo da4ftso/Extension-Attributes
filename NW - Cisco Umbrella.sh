822ce8d32ef19d02cc14dc4e4d183c5b16765658
da4 <da4@accessdenied.org>
2026-09-21 12:34:00 -0500

    Update NW - Cisco Umbrella.sh
    specifies which component was detected

--- NW - Cisco Umbrella.sh
+++ NW - Cisco Umbrella.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 
-# check for Cisco Umbrella components
+# 1.0.1 check for Cisco Umbrella components
 # regardless of Umbrella status (detected, running, extension loaded)
 
-if /usr/bin/systemextensionsctl list 2>/dev/null | \
-    /usr/bin/grep -qiE 'com\.cisco\.anyconnect\.macos\.acsockext\b' || \
-   [ -d "/opt/cisco/secureclient/umbrella" ] || \
-   [ -f "/opt/cisco/secureclient/bin/UmbrellaDiagnostic.app" ]; then
+# revert to previous for a simple Installed / Not Installed
 
-    result="Installed"
-else
-    result="Not Installed"
+result="Not Installed"
+
+if /usr/bin/systemextensionsctl list 2>/dev/null | \
+   /usr/bin/grep -qiE 'com\.cisco\.anyconnect\.macos\.acsockext\b'
+then
+    result="Installed (Socket Extension)"
+elif [ -d "/opt/cisco/secureclient/umbrella" ]
+then
+    result="Installed (Umbrella Directory)"
+elif [ -f "/opt/cisco/secureclient/bin/UmbrellaDiagnostic.app" ]
+then
+    result="Installed (UmbrellaDiagnostic.app)"
 fi
 
 echo "<result>${result}</result>"
-
-exit
